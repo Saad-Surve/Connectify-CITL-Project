a76c57023bcec26cc5bee16c485be4a7532344d3
Yusufsodawala786 <youzarsifsodawala7@gmail.com>
2023-10-02 13:41:26 +0530

    Some backend Work

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,8 @@ import * as z from "zod";
  import {Textarea} from "@/components/ui/textarea";
  import {isBase64Image} from "@/lib/utils";
  import {useUploadThing} from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
  interface Props {
   user: {
@@ -32,10 +34,11 @@ import * as z from "zod";
   btnTitle: string;
 }
 
-
  const AccountProfile = ({user, btnTitle}: Props) => {
    const [files, setFiles] = useState<File[]>([])
    const { startUpload } = useUploadThing("media")
+   const router = useRouter();
+   const pathname = usePathname();
    const form = useForm({
      resolver: zodResolver(UserValidation),
      defaultValues: {
@@ -75,7 +78,21 @@ import * as z from "zod";
       }
      }
 
-     // Update user profile
+     await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path:pathname,
+     });
+
+     if(pathname === '/profile/edit'){
+        router.back();
+     }
+     else {
+      router.push('/');
+     }
    }
 
    return(
